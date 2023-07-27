0774ecc3e91ddc1c59421eabf8de0a03e0354c77
Eduardo C <eduardo.cavazos1@wipro.com>
2023-07-27 14:17:11 -0600

    added webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
 
 new webpack.DefinePlugin({
     'process.env':{
