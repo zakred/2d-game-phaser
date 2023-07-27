69edd1e2b863a6c18bc038ecc8709030904b2ef4
Eduardo C <eduardo.cavazos1@wipro.com>
2023-07-27 13:58:48 -0600

    added loader for ts

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,24 +1,33 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './src/game.ts',
-  output: {
-    filename: 'game.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: true,
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ]
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './src/game.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    output: {
+        filename: 'game.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: true,
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+    ]
 }
