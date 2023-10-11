1b3367423dc753c79f5aec09b558ec52d393ae7e
Eduardo C <eduardo.cavazos1@wipro.com>
2023-10-10 18:11:53 -0600

    added tests with jest, improved structure and scripts for server

---
+++ server/src/test/room.test.ts
@@ -0,0 +1,14 @@
+import 'jest';
+import {createRoom, activeRooms} from '../main/room';
+
+describe('Room', () => {
+
+    beforeEach(() => {
+
+    });
+
+    it('should create a room', async () => {
+        createRoom('id', 'dummy')
+        expect(activeRooms.length).toEqual(1)
+    });
+});
